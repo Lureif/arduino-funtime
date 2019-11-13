cb147b7d8d18f23776bff3890b0575c284213749
Lureif <lureif.me@gmail.com>
2019-11-13 20:54:46 +0100

    little cheeky test

--- glowing_leds/glowing_leds.ino
+++ glowing_leds/glowing_leds.ino
@@ -4,11 +4,13 @@
 
 #define LED_PIN A3
 #define NUM_LEDS 200
+#define BRIGHTNESS 50
 CRGB	leds[NUM_LEDS];
 
 void	setup()
 {
 	FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+	FastLED.setBrightness(BRIGHTNESS);
 }
 
 void	loop()
