1b5180d50fae684878a77ffb5f5cd9460d6e341a
Lureif <lureif.me@gmail.com>
2019-11-13 21:22:03 +0100

    a

--- glowing_leds/glowing_led_name.ino
+++ glowing_leds/glowing_led_name.ino
@@ -17,42 +17,6 @@ void	setup()
 void	loop()
 {
 	unsigned int i;
-	unsigned int r;
-	unsigned int g;
-	unsigned int b;
-	
-	i, r, g, b = 0;
-	while (i <= NUM_LEDS)
-	{
-		r = abs(rand() % 100 - rand() % 100);
-		g = abs(rand() % 100 - rand() % 100);
-		b = abs(rand() % 100 - rand() % 100);
-		leds[i] = CRGB(r, g, b);
-		FastLED.show();
-		delay(500);
-		i++;
-	}
-}
-
-#include <FastLED.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-#define LED_PIN A3
-#define NUM_LEDS 200
-#define BRIGHTNESS 50
-
-CRGB	leds[NUM_LEDS];
-
-void	setup()
-{
-	FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
-	FastLED.setBrightness(BRIGHTNESS);
-}
-
-void	loop()
-{
-	unsigned int i;
 	unsigned int str_counter;
 	unsigned int r;
 	unsigned int g;
